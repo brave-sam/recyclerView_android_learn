0ec791e3470b2021117cca65f8e1174a1ddacd8b
Sam <77420012+sam0p34@users.noreply.github.com>
2023-02-14 00:10:03 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String[] arr={"areesha","areesha","areesha","areesha","areesha","areesha","areesha","areesha","areesha","areesha","areesha","areesha"};
+   RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        CustomAdapter c = new CustomAdapter(arr);
+        recyclerView.setAdapter(c);
+    }
+}
